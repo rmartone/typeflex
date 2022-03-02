a8e852bc0f33140ec3389b12de6a868a554d49a0
Ray Martone <rmartone@gmail.com>
2022-03-01 20:51:39 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # typeflex
+
---
+++ src/ygconfig.ts
@@ -0,0 +1,31 @@
+import {
+    YGLogger,
+    YGCloneNodeFunc
+} from "./yoga";
+
+const kYGDefaultExperimentalFeatures: () => Array<boolean> = () => [false, false, false];
+
+class YGConfig {
+    public experimentalFeatures: Array<boolean>;
+    public useWebDefaults: boolean;
+    public useLegacyStretchBehaviour: boolean;
+    public shouldDiffLayoutWithoutLegacyStretchBehaviour: boolean;
+    public pointScaleFactor: number;
+    public logger: YGLogger;
+    public cloneNodeCallback: YGCloneNodeFunc = null;
+    public context: any;
+
+    constructor(logger: YGLogger) {
+        this.experimentalFeatures = kYGDefaultExperimentalFeatures();
+        this.useWebDefaults = false;
+        this.useLegacyStretchBehaviour = false;
+        this.shouldDiffLayoutWithoutLegacyStretchBehaviour = false;
+        this.pointScaleFactor = 1.0;
+        this.logger = logger;
+        this.context = null;
+    }
+}
+
+export {
+    YGConfig
+};
